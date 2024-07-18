9a921e490c579d2608bcc8d8c78adb3ab09f647e
Jhoan Manuel Buitrago Chavez <jhoanuitrago@gmail.com>
2024-07-18 14:43:50 -0500

    fix error handling

--- chat-back/app/channels/messages_channel.rb
+++ chat-back/app/channels/messages_channel.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
+
 class MessagesChannel < ApplicationCable::Channel
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def subscribed
-    # TODO: handle error when no params are present
     receiver = User.find_by!(email: params[:receiver_email])
 
-    @conversation_id = UsersConversation.select('conversation_id').where(user_id: [current_user.id, receiver.id])
-                                        .group('conversation_id').having('COUNT(DISTINCT user_id) = 2')
-                                        .pick(:conversation_id)
+    @conversation_id = UsersConversation.find_conversation_id(current_user, receiver)
 
     if @conversation_id.nil?
       @conversation_id = Conversation.create.id
@@ -17,9 +15,14 @@ class MessagesChannel < ApplicationCable::Channel
     end
 
     stream_from "conversation_#{@conversation_id}"
+  rescue StandardError => e
+    # rescue_from does not work correctly with the `subscribed` method: https://github.com/rails/rails/issues/51855
+    rescue_with_handler(e)
   end
 
   def receive(data)
+    raise ActiveRecord::RecordNotFound if data['body'].include?('error')
+
     body = data['body']
     message = Message.create(conversation_id: @conversation_id, user_id: current_user.id, body:)
     ActionCable.server.broadcast("conversation_#{@conversation_id}", message)
@@ -29,17 +32,18 @@ class MessagesChannel < ApplicationCable::Channel
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def record_not_found
-    Rails.logger.info 'User not found. Rejecting...'
-    transmit({ error: "User '#{params[:receiver_id]}' not found." })
-    reject
+  # We can create methods that the frontend can call by using the `perform` method.
+  # For example: This method returns the conversation id.
+  def conversation_id
+    transmit({ conversation_id: @conversation_id })
   end
 
-  # We can create methods that the frontend can call by using the `perform` method.
+  private
 
-  # For example: This method returns the conversation id.
-  def connection_id
-    transmit({ conversation_id: @conversation_id })
+  def record_not_found(e)
+    Rails.logger.info 'User not found.'
+    transmit({ error: "User '#{params[:receiver_id]}' not found." })
+    reject
   end
 
 end
