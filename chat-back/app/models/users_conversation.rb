344639860bb262ca244159595e88e4b66232533a
Jhoan Manuel Buitrago Chavez <jhoanuitrago@gmail.com>
2024-07-18 14:46:38 -0500

    move conversation query to user model

--- chat-back/app/models/users_conversation.rb
+++ chat-back/app/models/users_conversation.rb
@@ -5,6 +5,13 @@ class UsersConversation < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
 
+  # Find the conversation_id for the conversation between the current_user and the receiver.
+  def self.find_conversation_id(current_user, receiver)
+    UsersConversation.select('conversation_id').where(user_id: [current_user.id, receiver.id])
+                     .group('conversation_id').having('COUNT(DISTINCT user_id) = 2')
+                     .pick(:conversation_id)
+  end
+
   def inspect
     "<UserConversation user_id: #{user_id}, conversation_id: #{conversation_id}>"
   end
